9a4d69e19ca81e622169552bc0f769e8859b212d
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-12 15:01:07 +0900

    feat: 포메팅 관련 설정 진행

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 # 페어프로그래밍 계획
 
 ## 1월 12일 (월)
+
 - 프로젝트 세팅
   - react + vite + typescript
   - 스타일링
@@ -12,11 +13,16 @@
     - eslint, prettier, husky
   - 서버
     - msw (mocking)
-  - 
+  -
+
 ## 1월 13일 (화)
-  - Mock 서버
-    - msw (mocking)
-      - [관련 글](https://velog.io/@khy226/msw%EB%A1%9C-%EB%AA%A8%EC%9D%98-%EC%84%9C%EB%B2%84-%EB%A7%8C%EB%93%A4%EA%B8%B0#service-worker%EB%9E%80)
+
+- Mock 서버
+  - msw (mocking)
+    - [관련 글](https://velog.io/@khy226/msw%EB%A1%9C-%EB%AA%A8%EC%9D%98-%EC%84%9C%EB%B2%84-%EB%A7%8C%EB%93%A4%EA%B8%B0#service-worker%EB%9E%80)
+
 ## 1월 14일 (수)
+
 ## 1월 15일 (목)
+
 ## 1월 16일 (금)
--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,40 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierPlugin from 'eslint-plugin-prettier';
+import sortImports from 'eslint-plugin-simple-import-sort';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      prettier: prettierPlugin,
+      'simple-import-sort': sortImports,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-var': 'error',
+      'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+      'no-unused-vars': 'error',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^\\u0000'],
+            ['^node:'],
+            ['^react'],
+            ['^@?\\w'],
+            ['^@/'],
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          ],
+        },
+      ],
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,4 +46,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+]);
