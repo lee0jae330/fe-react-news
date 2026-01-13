09dfb7cb663f40dd01da948c11aa6038f75d3c20
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-13 17:13:39 +0900

    feat: 전체언론사 선택 탭 추가

---
+++ src/4-features/news/ui/TotalPressTab.tsx
@@ -0,0 +1,15 @@
+interface TotalPressTabProps {
+  isSelected: boolean;
+  onClick: () => void;
+}
+
+export const TotalPressTab = ({ isSelected, onClick }: TotalPressTabProps) => {
+  return (
+    <button
+      className={`${isSelected ? 'selected-bold-16 text-text-strong' : 'available-medium-16 text-text-weak'}`}
+      onClick={onClick}
+    >
+      전체 언론사
+    </button>
+  );
+};
