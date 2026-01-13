28868c310e39fd24e4a546f5f55b114902dd2b02
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-13 16:29:44 +0900

    feat: rolling section 추가

---
+++ src/3-widgets/news/ui/RollingSection.tsx
@@ -0,0 +1,10 @@
+import { RollingItem } from '@/4-features/news';
+
+export const RollingSection = () => {
+  return (
+    <section className="flex gap-2">
+      <RollingItem />
+      <RollingItem />
+    </section>
+  );
+};
