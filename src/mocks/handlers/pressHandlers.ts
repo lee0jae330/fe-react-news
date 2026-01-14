73efb1410164026a8e3da6497601d8701d5150f6
lwjmcn <clarecse02@gmail.com>
2026-01-14 17:59:54 +0900

    fix: query parameter 제거 (msw: 'Do not include query parameters in your request predicate.')

--- src/mocks/handlers/pressHandlers.ts
+++ src/mocks/handlers/pressHandlers.ts
@@ -33,19 +33,18 @@ export const pressHandlers = [
   }),
 
   // 리스트 뷰 카테고리별 언론사 목록 및 주요 기사
-  http.get('/api/presses/list', ({ request }) => {
-    const url = new URL(request.url);
-    const category = url.searchParams.get('category');
+  http.get('/api/presses/list/categories/:categoryName', ({ params }) => {
+    const { categoryName } = params;
 
-    if (!category) {
-      return new HttpResponse('category is required', { status: 400 });
+    if (!categoryName) {
+      return new HttpResponse('categoryName is required', { status: 400 });
     }
-    if (!categories.includes(category)) {
-      return new HttpResponse('invalid category', { status: 400 });
+    if (!categories.includes(categoryName as string)) {
+      return new HttpResponse('invalid categoryName', { status: 400 });
     }
 
     const pressesInCategory = presses
-      .filter((p: Press) => p.category === category)
+      .filter((p: Press) => p.category === categoryName)
       .map((p: Press) => ({
         pressId: p.press,
         pressName: p.press,
