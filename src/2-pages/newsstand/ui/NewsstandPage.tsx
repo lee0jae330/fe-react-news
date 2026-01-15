996abc2478bf4f4f97782c441816ef5709d665ee
lwjmcn <clarecse02@gmail.com>
2026-01-15 11:03:06 +0900

    style: 전체 레이아웃 마진 설정

--- src/2-pages/newsstand/ui/NewsstandPage.tsx
+++ src/2-pages/newsstand/ui/NewsstandPage.tsx
@@ -3,7 +3,7 @@ import { NewsStandHeader, NewsStandMain } from '@/3-widgets/news';
 export const NewsstandPage = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <div className="flex h-180 w-7xl flex-col gap-10">
+      <div className="flex h-180 w-232.5 flex-col gap-10">
         <NewsStandHeader />
         <NewsStandMain />
       </div>
