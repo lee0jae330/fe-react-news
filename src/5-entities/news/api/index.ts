6e822d911d95149c715d4b62b20d19ddde194cd8
lwjmcn <clarecse02@gmail.com>
2026-01-15 12:14:20 +0900

    feat: 테스트 파일 삭제

--- src/5-entities/news/api/index.ts
+++
@@ -1,1 +0,0 @@
-export { getSubscriptions } from './subscriptions';
--- src/5-entities/news/api/subscriptions.ts
+++
@@ -1,9 +0,0 @@
-export const getSubscriptions = async () => {
-  const response = await fetch('/api/presses/subscriptions');
-  if (!response.ok) {
-    throw new Error('Failed to fetch subscription count');
-  }
-  const data = await response.json();
-
-  return data;
-};
