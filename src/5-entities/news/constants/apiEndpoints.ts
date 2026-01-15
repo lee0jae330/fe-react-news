cf3a614f7f3287b486a531100bb8377c99b9887f
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-15 18:37:20 +0900

    feat: api 엔드포인트 상수 추가

--- src/5-entities/news/constants/apiEndpoints.ts
+++ src/5-entities/news/constants/apiEndpoints.ts
@@ -1,3 +1,6 @@
 export const API_ENDPOINTS = {
   LATEST_NEWS: '/api/presses/latest-news',
+  GRID_VIEW_PRESS_LIST: '/api/presses/grid',
+  LIST_VIEW_PRESS_LIST: '/api/presses/list/categories/:categoryName',
+  SUBSCRIPTIONS: '/api/presses/subscriptions',
 };
